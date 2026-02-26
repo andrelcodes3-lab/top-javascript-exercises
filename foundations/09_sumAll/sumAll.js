d923caeadfadf44c9eabdd7653e2006d991a2bf0
andrelcodes3-lab <andrelcodes3@gmail.com>
2026-02-26 10:01:33 +0200

    complete task 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  if (typeof a !== typeof 1 || typeof b !== typeof 1) {
+    return "ERROR";
+  }
+  if (a < 0 || b < 0) {
+    return "ERROR";
+  }
+  if (!(Number.isInteger(a) && Number.isInteger(b))) {
+    return "ERROR";
+  }
+  let min = a < b ? a : b;
+  let toAdd = [];
+  if (min === a) {
+    for (let i = a; i <= b; i++) {
+      toAdd.push(i);
+    }
+  }
+  if (min === b) {
+    for (let i = b; i <= a; i++) {
+      toAdd.push(i);
+    }
+  }
+  return toAdd.reduce((accumulator, currenVal) => {
+    return accumulator + currenVal;
+  }, 0);
 };
 
 // Do not edit below this line
