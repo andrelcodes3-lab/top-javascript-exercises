8ff265631c5d5e03040d837d47422223182bfbe1
andrelcodes3-lab <andrelcodes3@gmail.com>
2026-02-26 09:23:51 +0200

    Complete task 08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-};
+function isIn(element, omit) {
+  let filtered = omit.filter((i) => i === element);
+  return filtered.length == 0 ? false : true;
+}
 
+const removeFromArray = function (arr, ...omit) {
+  return arr.filter((i) => isIn(i, omit) == false);
+};
 // Do not edit below this line
 module.exports = removeFromArray;
